159f6a1eb9e5bcb8af1950e53bbc72c9b0dea480
Carlos Cuesta <carloscuesta@me.com>
2023-03-07 20:46:20 +0100

    ✨ Add `light` theme variant base

--- src/colors/index.ts
+++ src/colors/index.ts
@@ -1,13 +1,16 @@
 import dark from "./dark";
+import light from "./light";
 
-export type Variant = "dark";
+export type Variant = "dark" | "light";
 
-type Colors = typeof dark;
+type Colors = typeof dark | typeof light;
 
 const getColors = (theme: Variant): Colors => {
   switch (theme) {
     case "dark":
       return dark;
+    case "light":
+      return light;
     default:
       throw Error(`Colors missing for variant: ${theme}`);
   }
